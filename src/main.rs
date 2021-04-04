b2ccd1a71eb1f9dc4169594d73f9cad792bb1ed3
Christopher Biscardi <chris@christopherbiscardi.com>
2021-04-03 19:36:26 -0700

    Lazily initializing a PublicKey via the environment
    Rust statics are kind of like consts except while
    `const` declares an immutable *value*, `static`
    declares a global variable, which could be modified.
    Note that modifying a static is considered `unsafe`.
    
    In general if you can't decide which one to use, you
    probably want a `const` because they can be inlined
    and propogated around the program by the compiler.
    In this case, we want a `static` though, because we
    have to initialize it lazily, at runtime, rather
    than at compile time.
    
    To lazily initialize a static, we can use the
    [lazy_static](https://crates.io/crates/lazy_static)
    crate. This will also allow us to use non-const
    functions in our initialization, such as those from
    the `hex` and `ed25519_dalek` crates, as well as
    `std::env::var`.
    
    ```rust
    use ed25519_dalek::PublicKey;
    use lazy_static::lazy_static;
    use std::env;
    
    lazy_static! {
        static ref PUB_KEY: PublicKey = PublicKey::from_bytes(
            &hex::decode(
                env::var("DISCORD_PUBLIC_KEY")
                  .expect("Expected DISCORD_PUBLIC_KEY to be set in the environment")
            )
            .expect("Couldn't hex::decode the DISCORD_PUBLIC_KEY")
        )
        .expect("Couldn't create a PublicKey from DISCORD_PUBLIC_KEY bytes");
    }
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use ed25519_dalek::PublicKey;
 use lamedh_http::{
     lambda::{lambda, Context},
     IntoResponse, Request,
 };
+use lazy_static::lazy_static;
+use std::env;
 
 type Error =
     Box<dyn std::error::Error + Send + Sync + 'static>;
+
+lazy_static! {
+    static ref PUB_KEY: PublicKey = PublicKey::from_bytes(
+        &hex::decode(
+            env::var("DISCORD_PUBLIC_KEY")
+              .expect("Expected DISCORD_PUBLIC_KEY to be set in the environment")
+        )
+        .expect("Couldn't hex::decode the DISCORD_PUBLIC_KEY")
+    )
+    .expect("Couldn't create a PublicKey from DISCORD_PUBLIC_KEY bytes");
+}
 
 #[lambda(http)]
 #[tokio::main]
